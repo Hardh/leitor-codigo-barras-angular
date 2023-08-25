7b106a4c0619d347d3ffece29ffe861273aae276
Alisson Dias <alissondiascangere@gmail.com>
2023-08-25 00:24:22 -0300

    fix: - Adicionando PWA; - Adicionando enviroment;

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  testOffline: false,
+  appVersion: 1,
+  teliosApi: 'https://sheetdb.io/api'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  testOffline: false,
+  appVersion: 1,
+  teliosApi: 'https://sheetdb.io/api'
+};
